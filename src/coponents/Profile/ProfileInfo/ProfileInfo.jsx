3b8974b7473e2b8b86cd05f8633db00a11d6ad20
unknown <kapron4ik ssd>
2020-09-09 05:37:25 +0300

    setState

--- src/coponents/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/coponents/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
     if (!props.profile){
@@ -9,13 +10,13 @@ const ProfileInfo = (props) => {
 
     return (
         <div>
-            <div>
-                <img
-                    src='https://s.ftcdn.net/v2013/pics/all/curated/RKyaEDwp8J7JKeZWQPuOVWvkUjGQfpCx_cover_580.jpg?r=1a0fc22192d0c808b8bb2b9bcfbf4a45b1793687'></img>
-            </div>
+            {/*<div>*/}
+            {/*    <img*/}
+            {/*        src='https://s.ftcdn.net/v2013/pics/all/curated/RKyaEDwp8J7JKeZWQPuOVWvkUjGQfpCx_cover_580.jpg?r=1a0fc22192d0c808b8bb2b9bcfbf4a45b1793687'></img>*/}
+            {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
-                ava+description
+                <ProfileStatus status={"Hello my friends"}/>
             </div>
         </div>
 
