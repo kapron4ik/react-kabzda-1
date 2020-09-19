ed8c66ca7e4d58143b43692f6a188bff3ddd777a
unknown <kapron4ik ssd>
2020-09-19 09:04:53 +0300

    http put, local state

--- src/redux/Profile-reducer.js
+++ src/redux/Profile-reducer.js
@@ -1,8 +1,9 @@
-import {usersAPI} from "../api/api";
+import {profileAPI, usersAPI} from "../api/api";
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT';
 const SET_USERS_PROFILE = 'SET_USERS_PROFILE';
+const SET_STATUS = 'SET_STATUS';
 
 
 let initialState = {
@@ -12,7 +13,8 @@ let initialState = {
         {id: 3, message: 'Да как и вчера, ты все прекрастно понимаешь.', likesCount: 18},
     ],
     newPostText: 'bla-bla-car-dub',
-    profile: null
+    profile: null,
+    status: ""
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -37,6 +39,13 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: action.newText
             };
         }
+        case
+        SET_STATUS: {
+            return {
+                ...state,
+                status: action.status
+            };
+        }
         case SET_USERS_PROFILE: {
             return {
                 ...state,
@@ -51,10 +60,25 @@ const profileReducer = (state = initialState, action) => {
 
 export const addPostActionCreator = () => ({type: ADD_POST});
 export const setUserProfile = (profile) => ({type: SET_USERS_PROFILE, profile});
+export const setStatus = (status) => ({type: SET_STATUS, status});
 export const getUserProfile = (userId) => (dispatch) => {
     usersAPI.getProfile(userId)
         .then(response => {
-            dispatch (setUserProfile(response.data));
+            dispatch(setUserProfile(response.data));
+        });
+}
+export const getStatus = (userId) => (dispatch) => {
+    profileAPI.getStatus(userId)
+        .then(response => {
+            dispatch(setStatus(response.data));
+        });
+}
+export const updateStatus = (status) => (dispatch) => {
+    profileAPI.updateStatus(status)
+        .then(response => {
+            if (response.data.resultCode === 0){
+                dispatch(setStatus(status));
+            }
         });
 }
 
